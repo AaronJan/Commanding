0f42b02b3529eaf128438b51b303f6822fbb939b
AaronJan <aaronjan@qq.com>
2019-04-26 01:17:14 +0800

    polish error message

--- src/errors/CommandHasNoHandler.ts
+++ src/errors/CommandHasNoHandler.ts
@@ -3,7 +3,7 @@ import { Command } from "../interfaces";
 
 export class CommandHasNoHandler extends Error {
   constructor(command: Command) {
-    super(`You should specify a handler for command "${command.getName()}".`);
+    super(`Nothing happend. You should specify a handler for command <${command.getName()}>.`);
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
