46b132e550fc59253797d63c4560f1c44b9fd461
AaronJan <aaronjan@qq.com>
2019-04-26 18:00:19 +0800

    formatting

--- src/errors/MissingArgument.ts
+++ src/errors/MissingArgument.ts
@@ -1,6 +1,5 @@
 import { ArgRequirement } from "../interfaces";
 
-
 export class MissingArgument extends Error {
   constructor(requirement: ArgRequirement) {
     super(`Required argument "${requirement.name}" is missing.`);
